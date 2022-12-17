8b13d0343e3dc973ffdfb1fdc114795f6b9b9226
yarqui <108986486+yarqui@users.noreply.github.com>
2022-12-17 14:31:53 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import SimpleLightbox from 'simplelightbox';
+
+// РОЗМІТКА ДЛЯ КАРТОК
+//  ===================================
+// <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>;
+// =================================
